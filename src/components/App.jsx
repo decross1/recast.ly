62d7c36f83450b9e8f6129a4726d6f1e0a8bcac5
decross1 <decross1@me.com>
2017-12-13 19:22:57 -0800

    finished basic tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-var App = () => (
+/*var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
@@ -14,8 +14,45 @@ var App = () => (
       </div>
     </div>
   </div>
-);
+);*/
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.App = App;
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      videos: window.exampleVideoData, 
+      video: window.exampleVideoData[0],
+    }
+    this.handleVideoChange = this.handleVideoChange.bind(this);
+  }
+  handleVideoChange(prop) {
+    this.setState({video: prop});
+    // this.setState({videos: [prop].concat(this.state.slice(0, index).concat(this.state.slice(index+1))});
+  }
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.video}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} videochange={this.handleVideoChange} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// window.App = App;
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,21 +1,26 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+let VideoListEntry = (props) => {
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
+ let onTitleClick = function(e) {
+   props.handlevideochange(props.video);
+ }
+  
+  
+  
+  return (
+    <div className="video-list-entry media">
+      <div className="media-left media-middle">
+        <img className="media-object" key={props.video.id.videoId} src={props.video.snippet.thumbnails.default.url} alt="" />
+      </div>
+      <div className="media-body">
+        <div className="video-list-entry-title" onClick={onTitleClick}>{props.video.snippet.title}</div>
+        <div className="video-list-entry-detail">{props.video.snippet.description}</div>
+      </div>
+    </div>
+  );
+}
+
 VideoListEntry.propTypes = {
   video: React.PropTypes.object.isRequired
 };
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
 window.VideoListEntry = VideoListEntry;
